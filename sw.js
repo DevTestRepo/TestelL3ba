0065e5436d34cda38a84de35acb98bb1ea86894b
Helmy <mohamed.helmy@5d-vr.com>
2026-05-13 12:28:55 +0300

    .

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE_NAME = 'el-le3ba-v1';
-self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(['./', './index.html', './manifest.webmanifest']))); });
-self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))); });
